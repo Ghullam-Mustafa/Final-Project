7248090c546b36963d54fe5c91f92a83c1f55be3
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-12-02 11:22:59 +0500

    b

---
+++ app/components/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function navbar() {
+  return (
+    <>
+    <div className="">
+        <div className="">
+            <Image src={"/assets/logo.png"} width={100} height={100}/>
+        </div>
+        <div className="">
+
+        </div>
+        <div className=""></div>
+    </div>
+    </>
+  )
+}
